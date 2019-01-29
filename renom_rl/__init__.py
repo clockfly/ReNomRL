71eddda6503beccc487b21f4df78c10411b2dbe9
Mike Ogawa <mikeogawa@Mikes-MacBook-Pro.local>
2019-01-29 17:24:31 +0900

    removed unneccessary codes at init

--- renom_rl/__init__.py
+++ renom_rl/__init__.py
@@ -63,26 +63,3 @@ class AgentBase(object,metaclass=AgentMeta):
         this is used when creating new object
         """
         raise NotImplementedError("Need to call super('class',self).__init__(log_key,record=True)  ('class':Class Name.)")
-
-
-    # @property
-    # def history(self):
-    #     return [
-    #         {
-    #             "epoch": self._epoch_index[i],
-    #             "avg_loss": self._train_avg_loss_list[i],
-    #             "avg_train_reward": self._train_avg_reward_list[i],
-    #             "train_reward": self._train_reward_list[i],
-    #             "test_reward": self._test_reward_list[i]
-    #         }
-    #         for i in range(len(self._epoch_index))]
-    #
-    # def test(self, *args, **kwargs):
-    #     pass
-    #
-    # def _append_history(self, epoch, avg_loss, avg_reward, train_reward, test_reward):
-    #     self._epoch_index.append(epoch)
-    #     self._train_avg_loss_list.append(avg_loss)
-    #     self._train_avg_reward_list.append(avg_reward)
-    #     self._train_reward_list.append(train_reward)
-    #     self._test_reward_list.append(test_reward)
