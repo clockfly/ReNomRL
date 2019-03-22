0821c67386c3141b2cbe5647356da3218edaf10b
mikeogawa <ogawa.mike@gridsolar.jp>
2019-03-22 16:45:56 +0900

    Created Additional Module Directory

---
+++ renom_rl/utility/additional_modules/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import division, print_function
+
+from .orthogonal import Orthogonal #,set_initializer
+from renom.layers.function.parameterized import Model
+import renom.utility.initializer as init
